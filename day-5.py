4c8507b3db1e1db38b006daf44308525ea7b137f
jda5 <74459860+jda5@users.noreply.github.com>
2020-12-05 16:39:12 +0000

    Update day-5.py

--- day-5.py
+++ day-5.py
@@ -9,17 +9,17 @@ def file_reader(file):
 def binary_partitioning(binary, index, lower, upper):
     difference = (upper - lower) / 2
     if difference < 1:
-        if binary[index] in 'FL':
+        if binary[0] in 'FL':
             return lower
         else:
             return upper
-    if binary[index] in 'FL':
-        return binary_partitioning(binary, index + 1, lower, floor(lower + difference))
+    if binary[0] in 'FL':
+        return binary_partitioning(binary[1:], lower, floor(lower + difference))
     else:
-        return binary_partitioning(binary, index + 1, ceil(lower + difference), upper)
+        return binary_partitioning(binary[1:], ceil(lower + difference), upper)
 
 
-ids = [8 * binary_partitioning(seat, 0, 0, 127) + binary_partitioning(seat, 7, 0, 7) for seat in file_reader("plane.txt")]
+ids = [8 * binary_partitioning(seat, 0, 127) + binary_partitioning(seat, 0, 7) for seat in file_reader("plane.txt")]
 
 # First solution
 print(max(ids))
