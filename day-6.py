b7ca146176e49a25d338f68c25558150d0729128
jda5 <74459860+jda5@users.noreply.github.com>
2020-12-06 12:16:13 +0000

    Add files via upload

---
+++ day-6.py
@@ -0,0 +1,17 @@
+# --- Solution 1 ---
+print(sum([len(set(group)) for group in [x.replace('\n', '') for x in open("declaration.txt", 'r').read().split('\n\n')]]))
+
+# --- Solution 2 ---
+
+from collections import Counter
+
+res = 0
+for group in [x.split('\n') for x in open("declaration.txt", 'r').read().split('\n\n')]:
+    shared = Counter()
+    for y in group:
+        for z in y:
+            shared[z] += 1
+    for value in shared.values():
+        if value == len(group):
+            res += 1
+print(res)
