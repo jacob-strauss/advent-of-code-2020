7de30fcc6e6fea0b3cf03ea10ec7c26b2ea535fd
jda5 <74459860+jda5@users.noreply.github.com>
2020-12-08 09:05:11 +0000

    Day 8

---
+++ day-8.py
@@ -0,0 +1,43 @@
+with open('instructions.txt', 'r') as file:
+    boot_code = file.readlines()
+
+
+# --- Solution 1 ---
+
+def run_code(code):
+    visited = []
+    acc = 0
+    i = 0
+
+    while i not in visited:
+        if i >= len(code):
+            return True, acc
+        visited.append(i)
+        op, arg = code[i].split()
+        if op == 'acc':
+            acc += int(arg)
+            i += 1
+        elif op == 'jmp':
+            i += int(arg)
+        else:
+            i += 1
+
+    return False, acc
+
+
+# --- Solution 2 ---
+
+def fix_code():
+    for index, line in enumerate(boot_code):
+        op, arg = line.split()
+        if op == 'acc':
+            continue
+        code_copy = boot_code[:]
+        if op == 'jmp':
+            code_copy[index] = f"nop {arg}"
+        else:
+            code_copy[index] = f"jmp {arg}"
+
+        res, acc = run_code(code_copy)
+        if res:
+            return acc
